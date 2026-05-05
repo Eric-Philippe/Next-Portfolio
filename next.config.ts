665007cf30326369afd288169b43f0aa4b813e11
Eric-Philippe <ericphlpp@gmail.com>
2026-05-05 11:47:15 +0200

    Remove unnecessary comments from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,3 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 import "./src/env.js";
 import createNextIntlPlugin from "next-intl/plugin";
 import createMDX from "@next/mdx";
